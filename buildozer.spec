1ff8e1c19385761204d614af51bfacc7579494cb
rudvadim@gmail.com <Putin_huilo1>
2017-08-08 12:49:26 +0300

    Added splash screen

--- buildozer.spec
+++ buildozer.spec
@@ -37,7 +37,7 @@ version = 0.0.1
 
 # (list) Application requirements
 # comma seperated e.g. requirements = sqlite3,kivy
-requirements = python3crystax, kivy, arrow, pyowm, plyer
+requirements = python3crystax, kivy, arrow, pyowm, plyer, kivymd
 
 # (str) Custom source folders for requirements
 # Sets custom source for any requirements with recipes
--- main.py
+++ main.py
@@ -6,13 +6,20 @@ from kivy.core.text import LabelBase
 
 
 class AddLocationForm(BoxLayout):
+
+
     def current_location_click(self):
         self.ids.notifications.text = 'click'
-
+        self.ids.act_prev.title = 'фівфів'
 
 
 class WeatherApp(App):
     kv_directory = 'templates'
+    title = 'Х*ва Погода'
+    def build(self):
+        root = AddLocationForm()
+        root.ids.act_prev.title = 'Х*ве меню'
+        return root
 
 
 
